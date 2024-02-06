2dacda737af86a3b25e1cd6ad96fe77b021093f0
Myo Myat Min <myomyatmin315@gmail.com>
2024-02-06 12:22:29 +0700

    complete login and logout routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,14 +2,6 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import generatedTokenAndSetCookie from "../utils/generateToken.js";
 
-export const loginUser = async (req, res) => {
-  try {
-    res.send("login");
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-
 export const signupUser = async (req, res) => {
   try {
     const { fullname, username, password, confirmPassword, gender } = req.body;
@@ -51,6 +43,36 @@ export const signupUser = async (req, res) => {
   }
 };
 
+export const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password." });
+    }
+
+    generatedTokenAndSetCookie(user._id, res);
+    res.status(200).json({
+      _id: user.id,
+      fullName: user.fullname,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 export const logoutUser = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully." });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
 };
