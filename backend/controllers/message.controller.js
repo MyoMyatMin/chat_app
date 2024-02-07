2a3500a751636fe30f509aefbf329d2db63b9b0f
Myo Myat Min <myomyatmin315@gmail.com>
2024-02-07 23:25:41 +0700

    get messages endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -33,3 +33,21 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatID } = req.params;
+    const senderID = req.user._id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderID, userToChatID] },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
