c91add27e8cf3d90f7a307bd03e2a4c4b92150ad
Myo Myat Min <myomyatmin315@gmail.com>
2024-02-19 20:00:58 +0700

    complete getConversations,getMessages,SearchConversations and sendMessages functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,6 +51,7 @@ export const loginUser = async (req, res) => {
       password,
       user?.password || ""
     );
+
     if (!user || !isPasswordCorrect) {
       return res.status(400).json({ error: "Invalid username or password." });
     }
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,17 +1,24 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
 import Messages from "./Messages";
 import MessagesInput from "./MessagesInput";
 import { TiMessages } from "react-icons/ti";
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
-          <div className="bg-slate-700 px-4 py-2 mb-2 flex">
+          <div className="bg-slate-800 px-4 py-2 mb-2 flex">
             <span className="label-text">To:</span>
-            <span className="text-gray-900 font-bold">John Doe</span>
+            <span className="text-white-900 font-bold">
+              {selectedConversation.fullname}
+            </span>
           </div>
           <Messages />
           <MessagesInput />
