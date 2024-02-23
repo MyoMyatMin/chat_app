4ae1e89eef0197c29726bc304ede4ecfb90599e3
Myo Myat Min <myomyatmin315@gmail.com>
2024-02-23 14:41:48 +0700

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3000", {
+      const socket = io("https://chat-app-mduy.onrender.com", {
         query: {
           userID: authUser._id,
         },
